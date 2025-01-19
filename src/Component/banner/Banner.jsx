c4d35ec143f540e8231aab08e1e085dc868cae8e
SujonRoyBD <sujonroy537527@gmail.com>
2025-01-19 23:46:17 +0600

    new

--- src/Component/banner/Banner.jsx
+++ src/Component/banner/Banner.jsx
@@ -14,7 +14,7 @@ const Banner = () => {
             <div className='grid grid-cols-1 md:grid-cols-2 px-12'>
                 <div className='mt-[100px]'>
                     <h3 className='text-6xl text-black font-bold'>this is garates the point  <strong className='text-red-600'>food</strong>, great <br />
-                        memories
+                        memorie
                     </h3>
                     <p> Enable diners to her customize their bookin Enable to make diners to customize their booking by
                         requesting this customies from here table he is those can make location or view.</p>
