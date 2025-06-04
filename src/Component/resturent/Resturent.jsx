624500d6c077cf5a660270be92ce8cd069ccca8c
SujonRoyBD <sujonroy537527@gmail.com>
2025-06-04 18:04:44 +0600

    129

--- src/Component/resturent/Resturent.jsx
+++ src/Component/resturent/Resturent.jsx
@@ -15,7 +15,7 @@ const Restuarent1 = () => {
         {
             img: "/assets/img/Food3.png",
             titel: "Skateboard Shoe",
-            price: "$125"
+            price: "$129"
 
         },
         {
